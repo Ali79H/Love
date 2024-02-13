d1e687379ec7506eb490e123dbc32bdf2a4af4b1
Ali79H <159948299+Ali79H@users.noreply.github.com>
2024-02-13 22:31:15 +0330

    Update pass.js

--- pass.js
+++ pass.js
@@ -2,7 +2,7 @@ const input = document.getElementById("inpt");
 const button = document.getElementById("btn");
 
 function check() {
-  if (input.value === "7979") {
+  if (input.value === "1230") {
     alert("تقدیم به بیب بیب خوشگلم");
     window.location.href = "main.html";
   } else {
